e572791ea7c17766cfbb3b0610e0ce5e8a964ea9
Sajjad Gul <sajjadarifgul@gmail.com>
2021-09-03 21:42:07 +0500

    Convert Excel to Data Table and Display in Table

--- Excellent/Controllers/HomeController.cs
+++ Excellent/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Excellent.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,23 +12,102 @@ namespace Excellent.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            return View(new HomeViewModel());
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(string sheetName = "")
         {
-            ViewBag.Message = "Your application description page.";
+            var model = new HomeViewModel() {
+                SheetName = sheetName
+            };
 
-            return View();
+            try
+            {
+                var file = Request.Files["excelFile"];
+                if (file != null && file.ContentLength > 0)
+                {
+                    model.FileName = file.FileName;
+
+                    string[] validFileTypes = { ".xls", ".xlsx" };
+                    string fileExtension = Path.GetExtension(file.FileName).ToLower();
+
+                    if (validFileTypes.Contains(fileExtension))
+                    {
+                        var uploadsFolderPath = Server.MapPath("~/Content/Uploads");
+                        if (!Directory.Exists(uploadsFolderPath)) Directory.CreateDirectory(uploadsFolderPath);
+
+                        string filePath = string.Format("{0}/{1}{2}", uploadsFolderPath, Guid.NewGuid(), fileExtension);
+
+                        file.SaveAs(filePath);
+
+                        string connString = "";
+                        if (fileExtension.Trim() == ".xls")
+                        {
+                            connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"";
+                        }
+                        else if (fileExtension.Trim() == ".xlsx")
+                        {
+                            connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
+                        }
+
+                        sheetName = !string.IsNullOrEmpty(sheetName) ? sheetName : "Sheet1";
+
+                        model.Data = ConvertExceltoDataTable(connString, sheetName);
+
+                        model.IsSuccessfull = true;
+                    }
+                    else
+                    {
+                        model.ErrorMessage = "Invalid File Format. Please upload a valid Excel file.";
+                    }
+                }
+                else
+                {
+                    model.ErrorMessage = "File not found. Please upload Excel file.";
+                }
+            }
+            catch (Exception ex)
+            {
+                model.ErrorMessage = ex.Message;
+            }
+
+            return View(model);
         }
 
-        public ActionResult Contact()
+        public static DataTable ConvertExceltoDataTable(string connString, string sheetName)
         {
-            ViewBag.Message = "Your contact page.";
+            DataTable dt = null;
 
-            return View();
+            OleDbConnection oledbConn = new OleDbConnection(connString);
+            try
+            {
+                oledbConn.Open();
+                using (OleDbCommand cmd = new OleDbCommand(string.Format("SELECT * FROM [{0}$]", sheetName), oledbConn))
+                {
+                    OleDbDataAdapter oleda = new OleDbDataAdapter();
+                    oleda.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    oleda.Fill(ds);
+
+                    dt = ds.Tables[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                var message = string.Format("It looks like Microsoft Access Database is not registered. It is required to establish connection with Excel and read file contents. Please install from here: https://www.microsoft.com/en-us/download/confirmation.aspx?id=13255. More Error Details: {0}", ex.Message);
+
+                throw new Exception(message);
+            }
+            finally
+            {
+                oledbConn.Close();
+            }
+
+            return dt;
         }
     }
 }
---
+++ Excellent/Models/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Excellent.Models
+{
+    public class HomeViewModel
+    {
+        public string FileName { get; set; }
+        public string SheetName { get; set; }
+        public bool IsSuccessfull { get; set; }
+        public string ErrorMessage { get; set; }
+        public DataTable Data { get; set; }
+    }
+}
